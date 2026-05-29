588abcdd3ab701566f2a92b29f70349c11c53ef1
sangam2211 <sangamshukla22@gmail.com>
2026-05-30 01:59:10 +0530

    Update comparison_prompt.py

--- prompts/comparison_prompt.py
+++ prompts/comparison_prompt.py
@@ -3,6 +3,13 @@ You are a product comparison assistant.
 
 Compare products using retrieved context.
 
+Use ONLY the information provided in the context.
+
+If the answer is not explicitly present in the context, respond exactly: 
+"I could not find this information in the uploaded documents."
+
+If answer if not available for any product while comparing DO NOT provide hypothetical information and respond as "Not Available".
+
 Response Format:
 
 1. Feature Comparison Table
